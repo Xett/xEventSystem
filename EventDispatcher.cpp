d2d546ffc06d876e6429317d62503c40f9071607
Xett <ethan.jones@my.jcu.edu.au>
2020-03-01 22:44:20 +1000

    Add EventDispatcher class

---
+++ EventDispatcher.cpp
@@ -0,0 +1,12 @@
+#include "EventDispatcher.h"
+#include "Event.h"
+
+EventDispatcher::EventDispatcher()
+{
+
+}
+
+Event EventDispatcher::DispatchEvent()
+{
+	return Event("");
+}
---
+++ EventDispatcher.h
@@ -0,0 +1,10 @@
+class Event;
+#ifndef EVENT_DISPATCHER_H
+#define EVENT_DISPATCHER_H
+class EventDispatcher
+{
+public:
+	EventDispatcher();
+	Event DispatchEvent();
+};
+#endif
